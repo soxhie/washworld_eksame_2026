0bfa39f3de393d806aec2a6077b9fc21a7f14885
Char <143526767+char667v@users.noreply.github.com>
2026-05-13 20:21:01 +0200

    rettelser step2 page

--- app/pages/onboarding/step2/page.tsx
+++ app/pages/onboarding/step2/page.tsx
@@ -5,7 +5,7 @@ import { FaArrowRight } from "react-icons/fa";
 import { FaChevronLeft } from "react-icons/fa";
 import { IoEyeOutline, IoEyeOffOutline } from "react-icons/io5";
 import { useState } from "react";
-export default function OnboardingStep2({ formData, updateFormData }) {
+export default function OnboardingStep2({ formData, updateFormData }: { formData: any, updateFormData: (data: any) => void }) {
     const [showPassword, setShowPassword] = useState(false);
     return (
         <div className="Onboarding-2">
@@ -59,14 +59,16 @@ export default function OnboardingStep2({ formData, updateFormData }) {
             <div className="toggleContainer">
                 <p>Jeg acceptere <a href="">Terms & Condtions</a></p>
                 <label className="switch">
-                    <input type="checkbox" required />
+                    <input type="checkbox" aria-required={true} />
+                    {/* <input type="checkbox" required /> */}
                     <span className="slider round"></span>
                 </label>
             </div>
             <div className="toggleContainer">
                 <p>Jeg acceptere <a href="">Privacy guidelines</a></p>
                 <label className="switch">
-                    <input type="checkbox" required />
+                    <input type="checkbox" aria-required={true} />
+                    {/* <input type="checkbox" required /> */}
                     <span className="slider round"></span>
                 </label>
             </div>

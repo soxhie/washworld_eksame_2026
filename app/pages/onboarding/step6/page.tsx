5a157b8ec0d0a0ebaf80a47abccf3cb540a2e968
sophi <sophi@example.com>
2026-05-18 13:28:34 +0200

    mobilepay and card toggle add in step6

--- app/pages/onboarding/step6/page.tsx
+++ app/pages/onboarding/step6/page.tsx
@@ -1,41 +1,47 @@
+"use client";
 import { CiCreditCard1 } from "react-icons/ci";
 import MobilePayInput from "../components/MobilepayInput";
+import { StepComponent } from "../components/stepsComponent";
 import CardInput from "../components/cardInput";
+import { useState } from "react";
 
 export default function OnboardingStep6() {
+    const [paymentMethod, setPaymentMethod] = useState("card");
 
-    const [formData, setFormData] = useState({
-        transaction_gateway_fk: "",
-    });
     return (
-        <div className="Onboarding-6">
-            <h1>Betalingsmetode</h1>
-            <div className="button">
-                <input
-                    type="radio"
-                    name="paymentMethod"
-                    id="card"
-                   
-                />
-                <CiCreditCard1 size={39} />
-                <div>
-                    <h3>Kort</h3>
-                    <h4>Visa/Mastercard</h4>
+        <>
+            <StepComponent currentStep={3} totalSteps={7} />
+            <div className="Onboarding-6">
+                <h1>Betalingsmetode</h1>
+                <div className="button">
+                    <input
+                        type="radio"
+                        name="paymentMethod"
+                        id="card"
+                        checked={paymentMethod === "card"}
+                        onChange={() => setPaymentMethod("card")}
+                    />
+                    <CiCreditCard1 size={39} />
+                    <div>
+                        <h3>Kort</h3>
+                        <h4>Visa/Mastercard</h4>
+                    </div>
                 </div>
+                <div className="button">
+                    <input
+                        type="radio"
+                        name="paymentMethod"
+                        id="mobilepay"
+                        checked={paymentMethod === "mobilepay"}
+                        onChange={() => setPaymentMethod("mobilepay")}
+                    />
+                    <CiCreditCard1 size={39} />
+                    <h3>MobilePay</h3>
+                </div>
+                {paymentMethod === "card" && <CardInput />}
+                {paymentMethod === "mobilepay" && <MobilePayInput />}
             </div>
-            <div className="button">
-                <input
-                    type="radio"
-                    name="paymentMethod"
-                    id="mobilepay"
-                  
-                />
-                <CiCreditCard1 size={39} />
-                <h3>MobilePay</h3>
-            </div>
-            {formData.transaction_gateway_fk === "card" && <CardInput />}
-            {formData.transaction_gateway_fk === "mobilepay" && <MobilePayInput />}
-        </div>
+        </>
     );
 }
 

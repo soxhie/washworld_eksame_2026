1d8be4810ff65853a09eb1027aa304e9e424fd7d
SophiaAnina <Sophia.anina@gmail.com>
2026-05-20 13:05:04 +0200

    Refactor onboarding steps and update styles for plan buttons

--- app/pages/onboarding/step3/page.tsx
+++ app/pages/onboarding/step3/page.tsx
@@ -1,29 +1,28 @@
 "use client";
 
 import { useState } from "react";
-import { FaPlus } from "react-icons/fa6";
-import { FaInfinity } from "react-icons/fa6";
-import { FaChevronRight } from "react-icons/fa";
+
+import { FaChevronRight,FaChevronLeft } from "react-icons/fa";
 import { FaArrowRight } from "react-icons/fa";
-import { FaChevronLeft } from "react-icons/fa";
-import { StepComponent } from "../components/stepsComponent";
-import CardInput from "../components/cardInput";
-
-
-export default function OnboardingStep6() {
-    const [paymentMethod, setPaymentMethod] = useState("card");
-
+import { GrGroup } from "react-icons/gr";
+import "../onboarding.css"
+export default function OnboardingStep5() {
+    const plans = [
+        { name: "guld", price: 139, description: "God og effectiv" },
+        { name: "premium", price: 169, description: "Extra Grundig" },
+        { name: "brilliant", price: 200, description: "Bedste vask året rundt" }
+    ];
+    const [clickedPlan, setClickedPlan] = useState<string | null>(null);
     return (
-       
-            
-            <div className="Onboarding-5">
-                <button
-                          className='tilbageLink'
-                          type="button"
-                        
-                        >
-                          <FaChevronLeft /> Tilbage
-                        </button>
+        
+            <div className="Onboarding-2">
+                 <button
+                        className='tilbageLink'
+                        type="button"
+                      >
+                        <FaChevronLeft /> Tilbage
+                      </button>
+                      
                 <h1>Vælg Abonnement</h1>
                 <p>Få ubegrænset bilvask til en fast lav pris og vask, hvor og hvornår det passer dig.</p>
                 {plans.map(plan => (
@@ -37,6 +36,7 @@ export default function OnboardingStep6() {
                             value={plan.name}
                             readOnly
                         />
+                        <GrGroup/>
                         <div>
                             <h3>{plan.name.charAt(0).toUpperCase() + plan.name.slice(1)}- {plan.price}kr./md.</h3>
                             <h4>{plan.description}</h4>
@@ -44,16 +44,13 @@ export default function OnboardingStep6() {
                         <FaChevronRight />
                     </button>
                 ))}
-                 <button
-                          className='nextButton'
-                          type="button"
-
-                        >
-                          <FaArrowRight />
-                        </button>
+              <button
+                      className='nextButton'
+                      type="submit"
+                    >
+                      <FaArrowRight />
+                    </button>
             </div>
-    
+        
     );
 }
-
-

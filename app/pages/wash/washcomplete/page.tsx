e6016bd13129c8b81d7aa7eec7b68bfe936c7a3d
Char <143526767+char667v@users.noreply.github.com>
2026-06-01 20:01:43 +0200

    layout rettelser washcomplete

--- app/pages/wash/washcomplete/page.tsx
+++ app/pages/wash/washcomplete/page.tsx
@@ -2,6 +2,7 @@
 
 import AppHeader from "../../../components/layout/AppHeader";
 // import BottomNav from "../../../components/layout/BottomNav";
+import "../wash.css";
 import { PiCarBold } from "react-icons/pi";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
@@ -13,12 +14,13 @@ export default function WashCompletePage() {
     <main style={{ minHeight: "100vh", overflowY: "auto", paddingBottom: 80, display: "flex", flexDirection: "column", alignItems: "center", color: "var(--background)" }}>
       <AppHeader variant="brand" />
       <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", flex: 1, gap: 24, padding: "40px 20px", marginBottom: 120 }}>
-        <h1 style={{ fontSize: "var(--h2-size)", fontWeight: 800, textAlign: "center", color: "var(--color-primary)" }}>Tak for dit besøg</h1>
-        <p style={{ fontSize: "var(--body-md-size)", color: "#fff", textAlign: "center", padding: "0 30px" }}>Programmet er nu afsluttet og vi ønsker dig en god tur videre</p>
+        <h1 style={{ fontSize: "var(--h2-size)", fontWeight: 800, textAlign: "center", color: "var(--color-primary)" }}>Program afsluttet </h1>
+         <Image src="/ChatGPTimage_conture_clear.png" alt="Bil" width={300} height={300} />
+         <h2 style={{ fontSize: "var(--h2-size)", color: "#fff", textAlign: "center", padding: "0 30px" }}>Tak for dit besøg</h2>
+        <p style={{ fontSize: "var(--body-md-size)", color: "#fff", textAlign: "center", padding: "0 30px" }}>Vi ønsker dig en god tur videre</p>
         {/* <PiCarBold style={{ fontSize: 120, color: "var(--color-primary)" }} /> */}
-        <Image src="/svg/bil_gron.svg" alt="Bil" width={100} height={100} />
         <div style={{ textAlign: "center" }}>{/* <h2 style={{ fontSize: "var(--h3-size)", fontWeight: 700 }}>Programmet er nu afsluttet</h2> */}</div>
-        <button onClick={() => router.push("/pages/dashboard")} style={{ color: "var(--color-primary)", background: "none", border: "none", cursor: "pointer", fontSize: "var(--body-md-size)", marginTop: "30px" }}>
+        <button onClick={() => router.push("/pages/dashboard")} style={{ color: "var(--color-primary)", background: "none", border: "none", cursor: "pointer", fontSize: "var(--body-md-size)", marginTop: "40px" }}>
           Tilbage til start
         </button>{" "}
       </div>
